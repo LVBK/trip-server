36258792c3c63ae1d16daec200e43a2091a2f6c2
LVBK <khanhlvb@ows.vn>
2016-04-20 22:03:46 +0700

    Trip schema

--- source/trips/both/collection/trips.js
+++ source/trips/both/collection/trips.js
@@ -30,6 +30,9 @@ LocationSchema.messages = {
     latOutOfRange: 'Latitude out of range' // Must be between -180 and 180
 };
 TripsSchema = new SimpleSchema({
+    owner: {
+        type: String
+    },
     origin: {
         type: LocationSchema,
         index: '2dsphere'
@@ -37,6 +40,242 @@ TripsSchema = new SimpleSchema({
     destination: {
         type: LocationSchema,
         index: '2dsphere'
+    },
+    isRoundTrip: {
+        type: Boolean
+    },
+    tripType: {
+        type: String,
+        allowedValues: ['one-time', 'often'],
+    },
+    travelDate: {
+        type: Date,
+        optional: true,
+        custom: function () {
+            if (this.field('tripType').value == 'one-time') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+            }
+        }
+    },
+    travelTime: {
+        type: Date,
+    },
+    returnDate: {
+        type: Date,
+        optional: true,
+        custom: function () {
+            if (this.field('isRoundTrip').value == true && this.field('tripType').value == 'one-time') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+            }
+        }
+    },
+    returnTime: {
+        type: Date,
+        optional: true,
+        custom: function () {
+            if (this.field('isRoundTrip').value == true) {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+            }
+        }
+    },
+    startDate: {
+        type: Date,
+        optional: true,
+        custom: function () {
+            if (this.field('tripType').value == 'often') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+            }
+        }
+    },
+    endDate: {
+        type: Date,
+        optional: true,
+        custom: function () {
+            if (this.field('tripType').value == 'often') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+            }
+        }
+    },
+    "travelDaysInWeek": {
+        type: Array,
+        minCount: 7,
+        maxCount: 7,
+        optional: true,
+        custom: function () {
+            if (this.field('tripType').value == 'often') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+                if (this.value.indexOf(true) == -1) {
+                    return 'noTravelDaysInWeek'
+                }
+            }
+        }
+    },
+    "travelDaysInWeek.$": {
+        type: Boolean,
+    },
+    "returnDaysInWeek": {
+        type: Array,
+        minCount: 7,
+        maxCount: 7,
+        optional: true,
+        custom: function () {
+            if (this.field('isRoundTrip').value == true && this.field('tripType').value == 'often') {
+                // inserts
+                if (!this.operator) {
+                    if (!this.isSet || this.value === null) return "required";
+                }
+                // updates
+                else if (this.isSet) {
+                    if (this.operator === "$set" && this.value === null) return "required";
+                    if (this.operator === "$unset") return "required";
+                    if (this.operator === "$rename") return "required";
+                }
+                if (this.value.indexOf(true) == -1) {
+                    return 'noReturnDaysInWeek'
+                }
+            }
+        }
+    },
+    "returnDaysInWeek.$": {
+        type: Boolean,
+    },
+    distance: {
+        type: Object,
+        blackbox: true
+    },
+    duration: {
+        type: Object,
+        blackbox: true
+    },
+    vehicle: {
+        type: String,
+        allowedValues: ['Motor', 'Car'],
+    },
+    seats: {
+        type: Number,
+        min: 2
+    },
+    emptySeaats: {
+        type: Number,
+        min: 1,
+        custom: function () {
+            if (this.value >= this.field('seats').value) {
+                return 'emptySeaatsOverSeats';
+            }
+        }
+    },
+    pricePerSeat: {
+        type: Number,
+        decimal: true,
+        min: 0
+    },
+    baggageSize: {
+        type: String,
+        allowedValues: ['Small', 'Medium', 'Large'],
+    },
+    flexibleTime: {
+        type: String,
+        allowedValues: ['On time', '+/- 5 minutes', '+/- 10 minutes', '+/- 15 minutes', '+/- 30 minutes'],
+    },
+    flexibleDistance: {
+        type: String,
+        allowedValues: ['Unacceptable', '3 kilometers', '5 kilometers', '10 kilometers', '15 kilometers'],
     }
+
 });
+TripsSchema.messages = {
+    noTravelDaysInWeek: 'Select at least one day of the week to travel', // Must be between -90 and 90
+    noReturnDaysInWeek: 'Select at least one day of the week to return', // Must be between -180 and 180
+    seatsOutOfRange: '[label] must be greater or equal than 2',
+    emptySeaatsOutOfRange: '[label] must be greater or equal than 1',
+    emptySeaatsOverSeats: '[label] must be less than seats value',
+    pricePerSeatOutOfRange: '[label] must be greater or equal than 0 $',
+};
 Trips.attachSchema(TripsSchema);
+SimpleSchema.messages({
+    required: "[label] is required",
+    minString: "[label] must be at least [min] characters",
+    maxString: "[label] cannot exceed [max] characters",
+    minNumber: "[label] must be at least [min]",
+    maxNumber: "[label] cannot exceed [max]",
+    minDate: "[label] must be on or after [min]",
+    maxDate: "[label] cannot be after [max]",
+    badDate: "[label] is not a valid date",
+    minCount: "You must specify at least [minCount] values",
+    maxCount: "You cannot specify more than [maxCount] values",
+    noDecimal: "[label] must be an integer",
+    notAllowed: "[value] is not an allowed value",
+    expectedString: "[label] must be a string",
+    expectedNumber: "[label] must be a number",
+    expectedBoolean: "[label] must be a boolean",
+    expectedArray: "[label] must be an array",
+    expectedObject: "[label] must be an object",
+    expectedConstructor: "[label] must be a [type]",
+    regEx: [
+        {msg: "[label] failed regular expression validation"},
+        {exp: SimpleSchema.RegEx.Email, msg: "[label] must be a valid e-mail address"},
+        {exp: SimpleSchema.RegEx.WeakEmail, msg: "[label] must be a valid e-mail address"},
+        {exp: SimpleSchema.RegEx.Domain, msg: "[label] must be a valid domain"},
+        {exp: SimpleSchema.RegEx.WeakDomain, msg: "[label] must be a valid domain"},
+        {exp: SimpleSchema.RegEx.IP, msg: "[label] must be a valid IPv4 or IPv6 address"},
+        {exp: SimpleSchema.RegEx.IPv4, msg: "[label] must be a valid IPv4 address"},
+        {exp: SimpleSchema.RegEx.IPv6, msg: "[label] must be a valid IPv6 address"},
+        {exp: SimpleSchema.RegEx.Url, msg: "[label] must be a valid URL"},
+        {exp: SimpleSchema.RegEx.Id, msg: "[label] must be a valid alphanumeric ID"}
+    ],
+    keyNotInSchema: "[key] is not allowed by the schema"
+});
--- source/trips/server/method/trips.js
+++ source/trips/server/method/trips.js
@@ -16,47 +16,11 @@ Meteor.methods({
         Future = Npm.require('fibers/future');
         var myFuture = new Future();
         try {
-            check(param.origin, LocationSchema);
-            check(param.destination, LocationSchema);
-            check(param.isRoundTrip, Boolean);
-            check(param.tripType, Match.Where(function (tripType) {
-                if (tripType !== 'one-time' && tripType !== 'often') {
-                    throw Meteor.Errpr('Invalid trip type');
-                } else
-                    return true;
-            }));
-            check(param.travelDate, Match.Optional(Date));
-            check(param.travelTime, Date);
-            check(param.returnDate, Match.Optional(Date));
-            check(param.returnTime, Match.Optional(Date));
-            check(param.startDate, Match.Where(function (startDate) {
-                if (param.tripType === 'often') {
-                    check(startDate, Date);
-                }
-                return true;
-            }));
-            check(param.endDate, Match.Where(function (endDate) {
-                if (param.tripType === 'often') {
-                    check(endDate, Date);
-                }
-                return true;
-            }));
-            check(param.travelOften, Match.Where(function (travelOften) {
-                if (param.tripType === 'often') {
-                    check(travelOften, [Boolean]);
-                    return travelOften.length == 7;
-                } else {
-                    return true;
-                }
-            }));
-            check(param.returnOften, Match.Where(function (returnOften) {
-                if (param.tripType === 'often' && param.isRoundTrip == true) {
-                    check(returnOften, ([Boolean]));
-                    return returnOften.length == 7;
-                } else {
-                    return true;
-                }
-            }));
+            if(!this.userId){
+                throw new Meteor.Error(406, "Login first");
+            }
+            param = _.extend({}, param, {owner: this.userId});
+            check(param, TripsSchema);
             if (param.hasOwnProperty('travelDate')) {
                 console.log(createATripDocument(param.origin, param.destination, param.travelDate, param.travelTime));
             }
@@ -66,32 +30,31 @@ Meteor.methods({
             if (param.hasOwnProperty('startDate') && param.hasOwnProperty('endDate')) {
                 if (param.isRoundTrip == true) {
                     for (i = param.startDate; i <= param.endDate; i.setDate(i.getDate() + 1)) {
-                        if (param.travelOften[i.getDay()]) {
+                        if (param.travelDaysInWeek[i.getDay()]) {
                             console.log(createATripDocument(param.origin, param.destination, i, param.travelTime));
                         }
-                        if (param.returnOften[i.getDay()]) {
+                        if (param.returnDaysInWeek[i.getDay()]) {
                             console.log(createATripDocument(param.destination, param.origin, i, param.returnTime));
                         }
                     }
                 } else {
                     for (i = param.startDate; i <= param.endDate; i.setDate(i.getDate() + 1)) {
-                        if (param.travelOften[i.getDay()]) {
+                        if (param.travelDaysInWeek[i.getDay()]) {
                             console.log(createATripDocument(param.origin, param.destination, i, param.travelTime));
                         }
                     }
                 }
             }
-            Trips.insert({
-                origin: param.origin,
-                destination: param.destination
-            }, function (err, result) {
-                if (err) {
-                    myFuture.throw(err);
-                    return myFuture.wait();
-                } else {
-                    myFuture.return(result);
-                }
-            })
+            Trips.insert(
+                param,
+                function (err, result) {
+                    if (err) {
+                        myFuture.throw(err);
+                        return myFuture.wait();
+                    } else {
+                        myFuture.return(result);
+                    }
+                })
         } catch (err) {
             console.log("createATrip: ", err);
             throw new Meteor.Error(407, err.reason || err.message);
