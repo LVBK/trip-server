3a06229403d314b4ba9894f8fc1668703c258b5d
LVBK <khanhlvb@ows.vn>
2016-04-25 01:00:28 +0700

    Basic search roadmap

--- source/trips/server/publish/trips.js
+++ source/trips/server/publish/trips.js
@@ -1,26 +1,59 @@
-Meteor.publish("trip_search", function (origin, destination, distance) {
-    var self = this, tripsIds, destinationIdSelector, destinationLocationSelector, destinationQuery={$and:[]};
+Meteor.publishComposite("trip_search", function (origin, destination, distance, date, limit) {
+    var self = this, roadMapIds, destinationIdSelector, destinationLocationSelector,
+        destinationQuery = {$and: []}, afterADay, dateQuery, normalizedLimit, base = 5;
     try {
-        if(origin && origin.length == 2){
-            tripsIds = Trips.find({
-                origin: {
-                    $near: {
-                        $geometry: {
-                            type: "Point",
-                            coordinates: [origin[0], origin[1]]
-                        },
-                        $maxDistance: distance
-                    }
-                }
-            }).map(function (trip) {
-                return trip._id;
+        check(distance, Match.Where(function (distance) {
+            check(distance, Number);
+            if (distance < 0) {
+                throw new Meteor.Error(505, "distance must be larger or equal than 0");
+            }
+            return distance >= 0;
+        }));
+        check(limit, Match.Optional(Number));
+        check(date, Date);
+        limit = limit || base;
+        normalizedLimit = limit + (base - (limit % base));
+        afterADay = new Date(date.getTime());
+        afterADay.setDate(afterADay.getDate() + 1);
+        dateQuery = {startAt: {$lt: afterADay, $gte: date}}
+        if (origin && origin.length == 2) {
+            roadMapIds = RoadMaps.find({
+                $and: [
+                    {
+                        origin: {
+                            $near: {
+                                $geometry: {
+                                    type: "Point",
+                                    coordinates: [origin[0], origin[1]]
+                                },
+                                $maxDistance: distance
+                            }
+                        }
+                    },
+                    dateQuery
+                ]
+            }).map(function (roadMap) {
+                var trip = Trips.findOne({$and: [{_id: roadMap.tripId}, {isDeleted: false}, {isFreezing: false}]});
+                if (!trip)
+                    return;
+                if (roadMap.slots && roadMap.slots.length == trip.seats)
+                    return;
+                return roadMap._id;
             });
-            destinationIdSelector = {_id: {$in: tripsIds}};
+
         } else {
-            destinationIdSelector = {};
+            roadMapIds = RoadMaps.find(dateQuery).map(function (roadMap) {
+                var trip = Trips.findOne({$and: [{_id: roadMap.tripId}, {isDeleted: false}, {isFreezing: false}]});
+                if (!trip)
+                    return;
+                if (roadMap.slots && roadMap.slots.length == trip.seats)
+                    return;
+                return roadMap._id;
+            });
         }
+        destinationIdSelector = {_id: {$in: roadMapIds}};
         destinationQuery.$and.push(destinationIdSelector);
-        if(destination && destination.length == 2){
+        if (destination && destination.length == 2) {
             destinationLocationSelector = {
                 destination: {
                     $near: {
@@ -36,9 +69,37 @@ Meteor.publish("trip_search", function (origin, destination, distance) {
             destinationLocationSelector = {};
         }
         destinationQuery.$and.push(destinationLocationSelector);
-        return Trips.find(destinationQuery);
-    } catch
-        (err) {
+        return {
+            find: function () {
+                Counts.publish(self, 'trip_search',
+                    RoadMaps.find(destinationQuery, {fields: {_id: 1}}), {noReady: true});
+                return RoadMaps.find(destinationQuery, {
+                    limit: normalizedLimit,
+                });
+            },
+            children: [
+                {
+                    find: function (roadMap) {
+                        return Trips.find({_id: roadMap.tripId}, {
+                            fields: {
+                                seats: 1,
+                                pricePerSeat: 1,
+                                vehicle: 1,
+                                owner: 1
+                            }
+                        });
+                    },
+                    children: [
+                        {
+                            find: function (trip) {
+                                return Meteor.users.find({_id: trip.owner}, {fields: {publicProfile: 1}});
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    } catch (err) {
         console.log("trip_search", err);
         self.ready();
         return;
